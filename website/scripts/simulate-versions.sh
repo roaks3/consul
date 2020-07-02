e890797cd1539559bf24889be1dd9b6c18d7572c
Ryan Oaks <roaks3@gmail.com>
2020-07-02 15:01:28 -0400

    Add 4 archived versions of docs

---
+++ website/scripts/simulate-versions.sh
@@ -0,0 +1,17 @@
+NUM_ARCHIVED_VERSIONS=4
+
+i=1
+while [ "$i" -le $NUM_ARCHIVED_VERSIONS ]; do
+  echo "Creating v$i"
+  mkdir content/api-docs-v$i
+  cp -R content/api-docs content/api-docs-v$i/.
+  mkdir content/docs-v$i
+  cp -R content/docs content/docs-v$i/.
+  mkdir content/intro-v$i
+  cp -R content/intro content/intro-v$i/.
+  i=$(( i + 1 ))
+done
+
+mv content/api-docs-v* content/api-docs
+mv content/docs-v* content/docs
+mv content/intro-v* content/intro
